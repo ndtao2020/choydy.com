40f170c2e805e6d3c2a86ea132acfea8c6d71907
Nguyễn Đình Tạo <ndtao2020@gmail.com>
2021-12-22 17:47:31 +0700

    fhbfghf

--- src/main/java/org/acme/base/dto/JwtToken.java
+++ src/main/java/org/acme/base/dto/JwtToken.java
@@ -1,14 +1,13 @@
 package org.acme.base.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.io.Serializable;
-
-@Data
-public class JwtToken implements Serializable {
-
-    private static final long serialVersionUID = 999944324545L;
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class JwtToken {
 
     @JsonProperty("access_token")
     private String access;
@@ -17,11 +16,38 @@ public class JwtToken implements Serializable {
     private String refresh;
 
     @JsonProperty("expires_in")
-    private long expires;
+    private Long expires;
+
+    public JwtToken() {
+    }
 
     public JwtToken(String accessToken, String refreshToken, long expires) {
         this.access = accessToken;
         this.refresh = refreshToken;
+        this.expires = expires;
+    }
+
+    public String getAccess() {
+        return access;
+    }
+
+    public void setAccess(String access) {
+        this.access = access;
+    }
+
+    public String getRefresh() {
+        return refresh;
+    }
+
+    public void setRefresh(String refresh) {
+        this.refresh = refresh;
+    }
+
+    public long getExpires() {
+        return expires;
+    }
+
+    public void setExpires(Long expires) {
         this.expires = expires;
     }
 }
