6fc2cc848632cb3ac2f77d3af47b2f7a1c5156ce
Nguyễn Đình Tạo <ndtao2020@gmail.com>
2022-01-18 17:33:57 +0700

    xzczx

--- webapp/src/layouts/main/components/RightSideBar.vue
+++ webapp/src/layouts/main/components/RightSideBar.vue
@@ -2,16 +2,21 @@
   <div class="right-sidebar-mini">
     <div class="right-sidebar-panel p-0">
       <div class="iq-card shadow-none">
-        <div class="p-0">
-          <img src="https://kinsta.com/wp-content/uploads/2020/06/half-page-1-1.png" width="250" height="600" />
-        </div>
+        <div class="p-0" v-html="renderAD()"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { rend } from '../rednerAD'
+
 export default {
-  name: 'MainRightSideBar'
+  name: 'MainRightSideBar',
+  methods: {
+    renderAD() {
+      return rend()
+    }
+  }
 }
 </script>
